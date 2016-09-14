68c022f68d6a70e5e0e2256a7be32176bd81aa62
Michael Covello <covelloz.michael@gmail.com>
2016-09-14 16:37:22 -0500

    update1
    Added some icons which get the directory location using get_src_path(). 
    Also updated the sample code to show how to write out an output file.

--- main.py
+++ main.py
@@ -28,7 +28,6 @@ import pandas as pd
 import numpy as np						
 import xlsxwriter as xl
 
-
 #Notification widget with progress bar
 class notify(QtGui.QWidget, progress_bar.Ui_notify):
 	
@@ -60,19 +59,18 @@ class worker(QThread):
 	
 	def run(self):
 
-		#Exit method if input fields are blank
+		#Error if input fields are blank
 		if(theShell.file_name == ''):
 			self.emit(SIGNAL("error_message()"))
 			
 		if(theShell.output_dir == ''):
 			self.emit(SIGNAL("error_message()"))
 		
+		#Read in a basic CSV or TXT file -- adjust parameters as needed
 		try:
-			#Read in a basic CSV or TXT file -- adjust parameters as needed
 			file_read = pd.read_csv(str(theShell.file_name),sep=',')
 		except Exception:
 			self.emit(SIGNAL("error_message()"))
-		
 		
 		#-------------------------------------------------------------------------#
 		'''Updates progress bar.
@@ -83,7 +81,6 @@ class worker(QThread):
 		#self.emit(SIGNAL("updateProgress()"))
 		#-------------------------------------------------------------------------#
 		
-		
 		try:
 			'''
 			INSERT HEAVY PROCESSING HERE
@@ -91,13 +88,20 @@ class worker(QThread):
 			
 			#--------------------------------------------------------------------------------#
 			#THIS IS A SAMPLE --> N = 3, i.e. progressBar.maxValue(3) in theShell.runReport()
+			#Using test_file.txt
+			
 			print("Testing the program")
 			
-			#print the column names (A, B, C):
+			#output file
+			output_file = open(os.path.normpath(theShell.output_dir + r"\\output_file.txt"), 'w')
+			
+			#print the column names (A, B, C) in the interpreter & write into an output file
 			for col in file_read.columns:
 				self.emit(SIGNAL("updateProgress()"))
 				print(col)
+				output_file.write("%s\n" % col)
 				time.sleep(2)
+			
 			#--------------------------------------------------------------------------------#
 			
 		except Exception:
@@ -117,12 +121,14 @@ class ListView(QtGui.QListWidget):
 		self.item(0).setFont(QtGui.QFont("Segoe UI", italic=True))
 		
 	def dragEnterEvent(self, event):
+		#Only accept drag event if mime data exists
 		if event.mimeData().hasUrls():
 			event.accept()
 		else:
 			event.ignore()
 	
 	def dragMoveEvent(self, event):
+		#Copy the mime data to memory while dragging
 		if event.mimeData().hasUrls:
 			event.setDropAction(QtCore.Qt.CopyAction)
 			event.accept()
@@ -130,6 +136,7 @@ class ListView(QtGui.QListWidget):
 			event.ignore()
 	
 	def dropEvent(self, event):
+		#Store the mime data into links array, pass it as a parameter into "dropped" signal.
 		if event.mimeData().hasUrls():
 			event.setDropAction(QtCore.Qt.CopyAction)
 			event.accept()
@@ -138,7 +145,7 @@ class ListView(QtGui.QListWidget):
 			for url in event.mimeData().urls():
 				links.append(str(url.toLocalFile()))
 			
-			self.emit(QtCore.SIGNAL("dropped"),links)
+			self.emit(QtCore.SIGNAL("dropped"), links)
 		else:
 			event.ignore()
 
@@ -161,24 +168,25 @@ class theShell(QtGui.QMainWindow, design.Ui_MainWindow):
 		#Calls setupUi from design.py file
 		self.setupUi(self)
 		
-		#Create listWidget with drag/drop ability
+		#Create listWidget instance with drag/drop ability
 		self.listWidget1 = ListView(self)
 		
 		#Connects signals (clicked/dropped) to slots		
 		self.btnFile.clicked.connect(self.selectFile)	
 		self.btnRUN.clicked.connect(self.runReport)
 		self.btnOutput.clicked.connect(self.selectFolder)
-		self.connect(self.listWidget1,QtCore.SIGNAL("dropped"),self.fileDropped)
+		self.connect(self.listWidget1,QtCore.SIGNAL("dropped"), self.fileDropped)
 		
 	def fileDropped(self, l):
+		#Take mime data data from drop event & add it to the list widgets
 		for url in l:
 			if os.path.exists(url):
 				self.listWidget1.clear()
 				self.listWidget2.clear()
-				self.listWidget1.addItem(QDir.toNativeSeparators(url))
-				self.listWidget2.addItem(QDir.toNativeSeparators(str(url.rsplit("/",1)[0])))
-				theShell.file_name = str(QDir.toNativeSeparators(url))
-				theShell.output_dir = str(QDir.toNativeSeparators(str(url).rsplit("/",1)[0]))
+				self.listWidget1.addItem(os.path.normpath(url))
+				self.listWidget2.addItem(os.path.normpath(str(url.rsplit("/",1)[0])))
+				theShell.file_name = str(os.path.normpath(url))
+				theShell.output_dir = str(os.path.normpath(str(url).rsplit("/",1)[0]))
 				
 	def selectFile(self):
 		#White-washes the list widgets & class variable references
@@ -188,17 +196,18 @@ class theShell(QtGui.QMainWindow, design.Ui_MainWindow):
 			theShell.file_name = ''
 			theShell.output_dir = ''
 			
-		#Opens file browser and allows user to choose a file
+		#Opens QFileDialog and allows user to choose a file; defaults to User's Home directory
 		file = QtGui.QFileDialog.getOpenFileName(self, "Select File", QDir.homePath())
 
 		#Adds items to list widgets & sets class variable references
-		#NOTE: output directory defaults to same directory in which the file resides
+		#NOTE: output directory defaults to same directory in which the chosen file resides
 		if file:
 			if self.sender().objectName() == "btnFile":
-				self.listWidget1.addItem(QDir.toNativeSeparators(file))
-				self.listWidget2.addItem(QDir.toNativeSeparators(str(file).rsplit("/",1)[0]))
-				theShell.file_name = str(QDir.toNativeSeparators(file))
-				theShell.output_dir = str(QDir.toNativeSeparators(str(file).rsplit("/",1)[0]))
+				#Must coerce file to string to avoid QString type-error when passing into os.path.normpath()
+				self.listWidget1.addItem(os.path.normpath(str(file)))
+				self.listWidget2.addItem(os.path.normpath(str(file).rsplit("/",1)[0]))
+				theShell.file_name = str(os.path.normpath(str(file)))
+				theShell.output_dir = str(os.path.normpath(str(file).rsplit("/",1)[0]))
 	
 	def selectFolder(self):
 		#White-washes the appropiate list widget
@@ -238,7 +247,6 @@ class theShell(QtGui.QMainWindow, design.Ui_MainWindow):
 		notify.want_to_close = True
 		theShell.msgProgress.close()
 
-	#Sets up and starts the worker thread
 	def runReport(self):
 		
 		#Display & setup notification widget
@@ -260,13 +268,30 @@ class theShell(QtGui.QMainWindow, design.Ui_MainWindow):
 		theShell.worker.start()
 		self.btnRUN.setEnabled(False)
 		
+def get_src_path():
+	#Application source path using cross-platform absolute paths separators: \\
+	
+	#If frozen using py-installer, files get stored into "...\\dist\\main"
+	#To get the icons folder, slice "\\dist\\main" off from the app_path.
+	if hasattr(sys, 'frozen'):
+		app_path = os.path.dirname(os.path.abspath(sys.executable)).split(os.sep)
+		return r"\\".join(app_path[:-2])
+	
+	#If running using a Python interpreter, no slicing is necessary.
+	else:
+		app_path = os.path.dirname(os.path.abspath(__file__)).split(os.sep)
+		return r"\\".join(app_path)
+
 #Main function		
 def main():
     #Create new instance of QApplication
 	app = QtGui.QApplication(sys.argv)
 	
+	#Get source path
+	source_path = get_src_path()
+	
 	#Sets application icon
-	#app.setWindowIcon(QtGui.QIcon('your/main_icon/location'))
+	app.setWindowIcon(QtGui.QIcon(os.path.normpath(source_path + r"\\icons\\main.png")))
 	
     #Creates instances of form windows
 	form = theShell()				#mainwindow
@@ -274,22 +299,21 @@ def main():
 	
     #Create message box instance to notify user upon completion
 	theShell.msgComplete = QMessageBox()
-	theShell.msgComplete.setText("The report has been generated.")
+	theShell.msgComplete.setText("The process has been completed!")
 	theShell.msgComplete.setWindowTitle("Process Complete!")
-	#theShell.msgComplete.setWindowIcon(QtGui.QIcon('/your/completed!_icon/location'))
+	theShell.msgComplete.setWindowIcon(QtGui.QIcon(os.path.normpath(source_path + r"\\icons\\done.png")))
 	
     #Create message box instance to notify user of an error
 	theShell.msgError = QMessageBox()
 	theShell.msgError.setText("An error has occurred.\nCheck that you selected the correct input file & output directory.")
 	theShell.msgError.setWindowTitle("Error!")
-	#theShell.msgError.setWindowIcon(QtGui.QIcon('/your/Error!_icon/location'))
+	theShell.msgError.setWindowIcon(QtGui.QIcon(os.path.normpath(source_path + r"\\icons\\error.png")))
 	
     #Show the mainwindow form	
 	form.show()
     
     #Execute the application	
 	app.exec_()
-
 
 #Calls main() function when executed in a Python shell	
 if __name__ == '__main__':
